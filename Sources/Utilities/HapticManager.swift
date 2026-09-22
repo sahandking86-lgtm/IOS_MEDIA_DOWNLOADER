f677812cec32ee669bfc8582cac4feac3c0c14ed
sahandking86-lgtm <sahandking86@gmail.com>
2026-09-23 02:19:06 +0300

    Add files via upload

---
+++ Sources/Utilities/HapticManager.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+enum HapticManager {
+    static func tap() {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+    }
+
+    static func success() {
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+    }
+
+    static func warning() {
+        UINotificationFeedbackGenerator().notificationOccurred(.warning)
+    }
+
+    static func error() {
+        UINotificationFeedbackGenerator().notificationOccurred(.error)
+    }
+}
